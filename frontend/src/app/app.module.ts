e95d6eb44f6560e58b79eaa83ba956ca62bca098
Chekwube <chekwube.udeogu@photizzo.com>
2019-06-18 15:01:41 +0100

    Temporarily fixed routes not found

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
